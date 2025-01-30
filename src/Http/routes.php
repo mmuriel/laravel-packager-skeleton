33cf81da016233bd95c5708e45e8e60ecbb4ccad
Mauricio Muriel <mauricio.muriel@calitek.net>
2025-01-30 17:00:31 -0500

    Ajustando definiciones en el modelo base y en el archivo de rutas

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,13 +1,13 @@
 <?php
 
 Route::group([
-        'prefix'        => 'admin/oficios',
+        'prefix'        => '/:lc:package',
         'middleware'    => ['web', 'user']
     ], function () {
 
-        Route::get('', 'Cameyando\Oficios\Http\Controllers\Oficios\OficiosController@index')->name('admin.oficios.index');
+        Route::get('', ':uc:vendor\:uc:package\Http\Controllers\:uc:packageController@index')->name(':lc:package.index');
         Route::get('/hello', function (){
             return "Hola Mundo!";
-        })->name('admin.oficios.hello');
+        })->name(':lc:package.hello');
 
 });
--- src/Models/MyPackage.php
+++ src/Models/MyPackage.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace :uc:vendor\:uc:package\\Models;
+namespace :uc:vendor\:uc:package\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use :uc:vendor\:uc:package\Contracts\:uc:package\ as :uc:package\Contract;
 
-class uc:package\ extends Model implements uc:package\Contract
+class :uc:package extends Model implements :uc:package\Contract
 {
     protected $fillable = [
     ];
